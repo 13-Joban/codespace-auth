25339854271d746e537318e3c580c3a56c7cb41b
Jobanpreet  Singh <jobanjatt113@gmail.com>
2022-11-26 07:04:06 +0000

    added app and index files

--- .devcontainer/index.js
+++ .devcontainer/index.js
@@ -1,1 +1,8 @@
-console.log("Hello from codespace")
+const app = require('./app.js')
+
+app.get('/', (req, res) => {
+    res.send('<h3>hi from server</h3>')
+})
+app.listen(process.env.PORT, () => {
+    console.log('app listening on ', process.env.PORT)
+})
